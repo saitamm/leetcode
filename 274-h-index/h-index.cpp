ae457c4e26b10ae4f1903afd60f94748f696548b
saitamm <151385133+saitamm@users.noreply.github.com>
2025-01-21 16:46:58 +0100

    Time: 0 ms (100.00%) | Memory: 12.3 MB (35.22%) - LeetSync

--- 274-h-index/h-index.cpp
+++ 274-h-index/h-index.cpp
@@ -2,8 +2,6 @@ class Solution {
 public:
     int hIndex(vector<int>& citations)
     {
-        if (citations.size() == 1 && citations[0] != 0)
-            return (1);
         sort(citations.begin(), citations.end(), greater<int>());
         for(int i = 0;i < citations.size();i++)
         {
