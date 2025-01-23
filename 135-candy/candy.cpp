7884bbe56e74715409f8fbbb829cc7405b773597
saitamm <151385133+saitamm@users.noreply.github.com>
2025-01-23 15:53:52 +0100

    Time: 125 ms (7.31%) | Memory: 23.5 MB (9.99%) - LeetSync

---
+++ 135-candy/candy.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings)
+    {
+        int min = 0;
+        vector<int> vector;
+        for(int i = 0;i < ratings.size();i++)
+            vector.push_back(1);
+        for(int i = 0;i < ratings.size() -1;i++)
+        {
+            if (ratings[i] < ratings[i+1])
+                vector[i+1] = vector[i] + 1;
+        }
+        for(int i = ratings.size() - 2; i >= 0;i--)
+        {
+            if (ratings[i] > ratings[i+1])
+                vector[i] += vector[i+1] - vector[i] + 1;
+            else if (vector[i+1] <= vector[i] && ratings[i] != ratings[i+1])
+                vector[i+1] = vector[i] + 1; 
+        }
+        for(int i = 0;i < ratings.size();i++)
+        {
+            printf(":;;%d\n", vector[i]);
+            min +=vector[i]; 
+        }
+        return (min);
+        
+    }
+};
