800c90da4cac0314ce0f4a16a13b1cd1d7797d73
saitamm <151385133+saitamm@users.noreply.github.com>
2025-01-24 10:25:49 +0100

    Time: 0 ms (100.00%) | Memory: 19.4 MB (91.62%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
@@ -3,21 +3,20 @@ public:
     vector<int> twoSum(vector<int>& numbers, int target)
     {
         vector<int> vec;
-
-        for(int i = 0;i < numbers.size();i++)
+        int j = numbers.size() -1;
+        int i = 0;
+        while (i < numbers.size() -1)
         {
-            // int 
-            for(int j = numbers.size() - 1;j >  i ;j--)
-            {
-                if (numbers[i] + numbers[j] == target)
+                if (numbers[i]+numbers[j] < target)
+                    i++;
+                else if (numbers[i] + numbers[j] == target)
                 {
                     vec.push_back(i+1);
                     vec.push_back(j+1);
                     return (vec);
                 }
-                else if (numbers[i]+numbers[j] < target)
-                    break;
-            }
+                else
+                    j--;
         }
         return(vec);
         
