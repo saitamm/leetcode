51c3acb84de595ba65a959211dce908c870083d0
saitamm <151385133+saitamm@users.noreply.github.com>
2025-01-30 12:09:59 +0100

    Time: 7 ms (20.75%) | Memory: 52.1 MB (84.96%) - LeetSync

---
+++ 55-jump-game/jump-game.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums)
+    {
+        int right = nums.size() -1;
+        int left = right - 1;
+        while (left >= 0)
+        {
+            if (nums[left] >= right - left)
+                right = left;
+            left--;
+        }
+        cout << "::" << right;
+        return(right == 0);
+    }
+};
