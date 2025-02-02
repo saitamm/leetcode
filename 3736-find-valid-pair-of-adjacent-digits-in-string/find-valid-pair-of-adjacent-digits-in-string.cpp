666033326610240028c4888b7f7c66ea9d5fc435
saitamm <151385133+saitamm@users.noreply.github.com>
2025-02-02 09:49:25 +0100

    Time: 4 ms (0.00%) | Memory: 8.9 MB (20.00%) - LeetSync

--- 3736-find-valid-pair-of-adjacent-digits-in-string/find-valid-pair-of-adjacent-digits-in-string.cpp
+++ 3736-find-valid-pair-of-adjacent-digits-in-string/find-valid-pair-of-adjacent-digits-in-string.cpp
@@ -9,7 +9,6 @@ public:
             hash[s[i]]++;
         for(int i =0;i< s.size() - 1 ;i++)
         {
-            cout << hash[s[i]] << " :: " <<hash[s[i+1]] <<"\n";
             if (abs(hash[s[i]])+48 == s[i] && abs(hash[s[i+1]]) + 48 == s[i+1] && s[i] != s[i+1])
             {
                 if (hash[s[i]]>0)
