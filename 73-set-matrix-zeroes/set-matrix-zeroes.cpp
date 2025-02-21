8884307be7da0216a7daa0ed3735632e2a91f746
saitamm <151385133+saitamm@users.noreply.github.com>
2025-02-21 21:20:27 +0100

    Time: 0 ms (100.00%) | Memory: 19.2 MB (6.80%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -4,10 +4,7 @@ public:
     {
         unordered_map <int ,vector<int>> map;
         for(int i = 0;i <matrix.size();i++)
-        {
             map[i] = matrix[i];
-        }
-
         for(int col = 0;col < matrix.size();col++)
         {
             for(int line = 0;line < matrix[0].size();line++)
@@ -15,25 +12,9 @@ public:
                 if (map[col][line] == 0)
                 {
                     for(int i = 0;i < matrix[0].size();i++)
-                    {
                         matrix[col][i] = 0;
-                    }
                     for(int j = 0;j < matrix.size();j++)
-                    {
                         matrix[j][line] = 0;
-                    }
-                    // int up = col-1;
-                    // int down = col+1;
-                    // int left = line -1;
-                    // int right = line +1;
-                    // if ( up >= 0 && matrix[up][line] != 0)
-                    //     matrix[up][line] = 0;
-                    //  if ( down < matrix.size() && matrix[down][line] != 0)
-                    //     matrix[down][line] = 0;
-                    //  if ( left >= 0 && matrix[col][left] != 0)
-                    //     matrix[col][left] = 0;
-                    //  if ( right < matrix[0].size() && matrix[col][right] != 0)
-                    //     matrix[col][right] = 0;
                 }
             }
         }
