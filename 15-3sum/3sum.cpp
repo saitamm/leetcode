c08d5c9c4b1aa5bb02c8f17500b8b2e699c691f1
saitamm <151385133+saitamm@users.noreply.github.com>
2025-02-23 10:18:17 +0000

    Time: 84 ms (24.01%) | Memory: 36 MB (20.72%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -6,6 +6,7 @@ public:
         sort(nums.begin(),nums.end());
         for(int i =0;i < nums.size() - 1;i++)
         {
+            if(i > 0 && nums[i] == nums[i-1]) continue;
             int left = i + 1;
             int right = nums.size()-1;
             while (left != right)
@@ -14,7 +15,6 @@ public:
                 {
                     reslt.insert({nums[i], nums[left], nums[right]});
                     left++;
-                    //break;
                 }
                 else if (nums[left] + nums[right] < -nums[i])
                     left++;
