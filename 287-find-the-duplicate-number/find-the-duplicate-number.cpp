8df47031d2820b59d2105c31bc73e65054a1f3a1
saitamm <151385133+saitamm@users.noreply.github.com>
2025-04-11 09:43:31 +0100

    Time: 75 ms (12.12%) | Memory: 87.8 MB (18.79%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -4,14 +4,11 @@ public:
     {
         unordered_map <int, int> hash;
         int size = nums.size();
-        for(int i = 0;i<=size/2;i++)
+        for(int i = 0;i<=size;i++)
         {
             hash[nums[i]]++;
-            hash[nums[size-i-1]]++;
             if (hash[nums[i]] > 1)
                 return (nums[i]);
-            else if (hash[nums[size-i-1]] >1)
-                return (nums[size -i-1]);
         }
         return (-1);
     }
