d231c8872a3b51437f4815db3065294a0fa004b7
saitamm <151385133+saitamm@users.noreply.github.com>
2025-05-11 09:18:43 +0100

    Time: 0 ms (100.00%) | Memory: 12.5 MB (46.71%) - LeetSync

--- 1293-three-consecutive-odds/three-consecutive-odds.py
+++ 1293-three-consecutive-odds/three-consecutive-odds.py
@@ -4,16 +4,8 @@ class Solution(object):
         :type arr: List[int]
         :rtype: bool
         """
-        for i in range(0, len(arr)):
-            if arr[i] % 2 != 0:
-                odd = 0 
-                for j in range(i, len(arr)):
-                    if (arr[j] %2 != 0):
-                        odd+=1
-                        i = j
-                    else:
-                        break
-                if (odd >= 3):
+        for i in range(0, len(arr)-2):
+            if arr[i] % 2 != 0 and arr[i+1] % 2!= 0 and arr[i+2] %2 != 0:
                     return (True)
         return (False)
         
