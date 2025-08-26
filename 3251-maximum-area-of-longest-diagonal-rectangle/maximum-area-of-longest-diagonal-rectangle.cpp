8aba7b433ba6296f0b0ea9f7c0b04e5466ca8dc7
saitamm <151385133+saitamm@users.noreply.github.com>
2025-08-26 10:52:31 +0100

    Time: 0 ms (100.00%) | Memory: 29.3 MB (52.12%) - LeetSync

--- 3251-maximum-area-of-longest-diagonal-rectangle/maximum-area-of-longest-diagonal-rectangle.cpp
+++ 3251-maximum-area-of-longest-diagonal-rectangle/maximum-area-of-longest-diagonal-rectangle.cpp
@@ -9,27 +9,20 @@ public:
         for(int i = 0;i<dimensions.size();i++)
         {
             Length = sqrt(dimensions[i][0]*dimensions[i][0]+dimensions[i][1]*dimensions[i][1]);
+            size_t area = dimensions[i][0]* dimensions[i][1];
             if (Length >= maxLength)
             {
-                if (idx >= 0)
-                {
-                    if (Length == maxLength && dimensions[i][0]* dimensions[i][1] > dimensions[idx][0]*dimensions[idx][1])
+                    if (Length == maxLength &&  area > dimensions[idx][0]*dimensions[idx][1] )
                     {
                         maxLength = Length ;
                         idx = i;
 
                     }
-                    else if(Length > maxLength)
+                    else if (Length > maxLength)
                     {
                         maxLength = Length;
                         idx = i;
                     }
-                }
-                else
-                {
-                    maxLength = Length ;
-                    idx = i;
-                }
             }
 
         }
