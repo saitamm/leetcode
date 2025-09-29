c8da34d44b2419e14cf66f196663e66aab45e3d2
saitamm <151385133+saitamm@users.noreply.github.com>
2025-09-29 17:59:34 +0100

    Time: 31 ms (25.54%) | Memory: 111.9 MB (9.34%) - LeetSync

---
+++ 739-daily-temperatures/daily-temperatures.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures)
+    {
+        stack<pair<int, int>> stk;
+        vector<int> answer(temperatures.size());
+        for(int idx= 0 ;idx < temperatures.size();idx++)
+        {
+            while (!stk.empty() && stk.top().second < temperatures[idx])
+            {
+                answer[stk.top().first] = idx - stk.top().first;
+                stk.pop();
+            }
+            stk.push({idx, temperatures[idx]});
+        }
+        // while (!stk.empty()) {
+        // cout << stk.top() << " ";
+        // stk.pop();
+        return (answer);
+    }
+};
