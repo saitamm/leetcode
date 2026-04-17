2e311b4f8899e87040a4025b5035aa3690386cef
saitamm <151385133+saitamm@users.noreply.github.com>
2026-04-17 16:00:16 +0100

    Time: 0 ms (100.00%) | Memory: 12.5 MB (29.41%) - LeetSync

--- 60-permutation-sequence/permutation-sequence.py
+++ 60-permutation-sequence/permutation-sequence.py
@@ -6,8 +6,10 @@ class Solution(object):
         :rtype: str
         """
         arr = list(range(1, n + 1))
-        perms = list(itertools.permutations(arr))
         result = ""
-        for i in range(0, len(perms[k-1])):
-            result += str(perms[k-1][i])
-        return result
+        while arr :
+            idx = (k-1)// (math.factorial(len(arr)-1))
+            result += str(arr[idx])
+            k = k % math.factorial(len(arr) - 1)
+            arr.remove(arr[idx])
+        return(result)
