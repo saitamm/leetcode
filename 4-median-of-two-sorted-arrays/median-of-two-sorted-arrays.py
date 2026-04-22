5bd0b149ee095ce62af3d8e60c03e08363f9d37f
saitamm <151385133+saitamm@users.noreply.github.com>
2026-04-22 10:17:56 +0100

    Time: 5 ms (31.67%) | Memory: 12.7 MB (20.37%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
@@ -10,18 +10,12 @@ class Solution(object):
         med1 = len(nums1)//2
         med2 = min(half - med1, len(nums2)-1)
         if len(nums1) != 0 and len(nums2) != 0 :
-            print("med1 = ", med1, "   nums1 = ", nums1)
-            print("med2 = ", med2, "   nums2 = ", nums2)
             for i in range(med1+1, len(nums1)):
-                print("*********", nums1[i])
                 if nums1[i] <= nums2[med2]:
                     med1+=1
             for i in range(med2+1, len(nums2)):
                 if nums2[i] <= nums1[med1]:
                     med2+=1
-        i = 0
-        j = 0
-        k = 0
         arr = nums1[0:med1+1]+nums2[0:med2+1]
         arr.sort()
         if length % 2 == 0:
