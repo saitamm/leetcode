b3b7c93719328e1c22184874ddf5aa8bd2b89c6b
saitamm <151385133+saitamm@users.noreply.github.com>
2026-04-28 10:26:57 +0100

    Time: 638 ms (67.75%) | Memory: 163.3 MB (13.38%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -6,12 +6,10 @@ class Solution:
         target = total_sum // 2
         @lru_cache(None)
         def dfs(i: int, target: int) -> bool:
-            # base cases
             if target == 0:
                 return True
             if i >= len(nums) or target < 0:
                 return False
-            # recursive exploration
             return dfs(i + 1, target - nums[i]) or dfs(i + 1, target)
 
         return dfs(0, target)
