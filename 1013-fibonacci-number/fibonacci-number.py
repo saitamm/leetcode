30b432a2e26a9f73bea92ddf96e48dc37f5f50aa
saitamm <151385133+saitamm@users.noreply.github.com>
2026-05-04 15:00:54 +0100

    Time: 8 ms (95.42%) | Memory: 12.4 MB (52.58%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -1,18 +1,13 @@
-def fibn(memo, n):
-        if (memo[n] != 0):
-            return (memo[n])
-        if(n == 1 or n == 2):
-            return (1)
-        else :
-            result = fibn(memo, n-1) + fibn(memo,  n-2)
-        memo[n] = result
-        return (result)
 class Solution(object):
-
     def fib(self, n):
-        if (n == 0):
-            return 0
-        arr = [0] * 100
-        result = fibn(arr, n)
-        return (result)
-        
+        """
+        :type n: int
+        :rtype: int
+        """
+        fb  = []
+        fb.append(0)
+        fb.append(1)
+        for i in range(2, n+1):
+            fb.append(fb[i-2] + fb[i-1])
+        print(fb)
+        return fb[n]
