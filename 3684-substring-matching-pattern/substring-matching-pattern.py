92cf60873055e910e11a87ea7c7a5c294dd4f00f
saitamm <151385133+saitamm@users.noreply.github.com>
2026-05-05 09:52:25 +0100

    Time: 0 ms (100.00%) | Memory: 12.3 MB (97.37%) - LeetSync

--- 3684-substring-matching-pattern/substring-matching-pattern.py
+++ 3684-substring-matching-pattern/substring-matching-pattern.py
@@ -10,12 +10,8 @@ class Solution(object):
             return True
         pos = p.find('*')
         first  = p[:pos]
-        print("first = ", first)
         second = p[pos+1:]
-        print("second = ", second)
         firstsub = s.find(first)
         result1 = s[firstsub+len(first):]
-        print("****** = ", firstsub)
         final = result1.find(second)
-        print("-------------", final)
         return firstsub != -1 and final != -1
